88f421c08e37a52e4603bbfa3edcc049cd443792
RichtersFinger <steffen.finger.privat@gmail.com>
2025-06-14 15:57:56 +0200

    [fe, components] add initial Badg-component

---
+++ frontend/src/components/base/Badge.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface BadgeProps {
+  className?: string;
+  children?: React.ReactNode;
+  onClick?: (event: React.MouseEvent) => void;
+}
+
+export default function Badge({
+  className = "",
+  children,
+  onClick,
+}: BadgeProps) {
+  return (
+    <div
+      className={`flex flex-row space-x-2 items-center px-2 py-1 bg-gray-300 rounded-xl text-nowrap ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+}
